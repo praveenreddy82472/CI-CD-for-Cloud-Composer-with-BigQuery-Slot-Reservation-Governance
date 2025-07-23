87295d9407b37fe0296395c154ef4dc0b730f6a7
Thumati Praveen Kumar <65601696+praveenreddy82472@users.noreply.github.com>
2025-07-23 11:30:21 -0500

    Update gcs_to_bq_dag.py

--- Dags/gcs_to_bq_dag.py
+++ Dags/gcs_to_bq_dag.py
@@ -1,24 +1,36 @@
 from airflow import DAG
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from datetime import datetime
+import os
+
+# Get the environment from ENV variable (set in Composer config)
+ENV = os.getenv("ENV", "dev")  # default to "dev"
+
+# Set variables based on environment
+if ENV == "prod":
+    gcs_uri = "gs://healthpra18/healthcare_dataset.csv"
+    bq_table = "health_table"
+else:
+    gcs_uri = "gs://healthpra18/Health_sample.csv"  # only 100 rows
+    bq_table = "health_sample"
 
 with DAG(
-    dag_id='gcs_to_bq_with_reservation',
+    dag_id=f'gcs_to_bq_{ENV}',  # makes dag_id unique per environment
     schedule_interval='@daily',
     start_date=datetime(2024, 1, 1),
     catchup=False,
-    tags=['ci', 'bq', 'composer']
+    tags=['ci', 'bq', 'composer', ENV]
 ) as dag:
 
     load_csv_to_bq = BigQueryInsertJobOperator(
         task_id='load_csv',
         configuration={
             "load": {
-                "sourceUris": ["gs://healthpra18/healthcare_dataset.csv"],
+                "sourceUris": [gcs_uri],
                 "destinationTable": {
                     "projectId": "consummate-fold-466316-c3",
                     "datasetId": "health",
-                    "tableId": "health_table"
+                    "tableId": bq_table
                 },
                 "sourceFormat": "CSV",
                 "writeDisposition": "WRITE_TRUNCATE",
@@ -28,4 +40,3 @@ with DAG(
         location="US",
         gcp_conn_id="google_cloud_default"
     )
-
